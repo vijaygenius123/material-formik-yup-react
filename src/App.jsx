94de8a388b5ab1059ed5e44be96e3977516848fb
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-11-13 20:18:19 +0000

    Update to react-router-dom v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Notes from "./pages/Notes";
 import Create from "./pages/Create";
 
 function App() {
     return (
         <Router>
-            <Switch>
-                <Route exact path="/" component={<Notes/>}></Route>
-                <Route path="/create" component={<Create/>}></Route>
-            </Switch>
+            <Routes>
+                <Route exact path="/" component={<Notes/>}/>
+                <Route path="/create" component={<Create/>}/>
+            </Routes>
         </Router>
     );
 }
