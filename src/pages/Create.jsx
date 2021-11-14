9503ac339dc10dabbb933f8eca362da471191fdf
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-11-14 21:36:31 +0000

    Add Material-ui button

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Typography from "@material-ui/core/Typography";
-
-function Create(props) {
+import Button from "@material-ui/core/Button";
+function Create() {
     return (
         <div>
-            <Typography variant="h1" align="center">
+            <Typography variant="h1" align="center" gutterBottom>
                 Create Note
             </Typography>
+            <Button type="submit" color="secondary" variant="contained">Submit</Button>
         </div>
     );
 }
